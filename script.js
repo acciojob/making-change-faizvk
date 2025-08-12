0abb893767192b91bd82248444010552961d2044
faizvk <faizvk14@gmail.com>
2025-08-13 01:56:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const makeChange = (c) => {
-  // your name here
+  c = parseInt(c, 10); 
+  
+  const change = {
+    q: 0,
+    d: 0,
+    n: 0,
+    p: 0
+  };
+
+  change.q = Math.floor(c / 25);
+  c %= 25;
+
+  change.d = Math.floor(c / 10);
+  c %= 10;
+
+  change.n = Math.floor(c / 5);
+  c %= 5;
+
+  change.p = c; 
+
+  return change;
 };
 
-// Do not the change the code below
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
